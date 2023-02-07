d4e9cb2d20589bc1448ce98f77bc7409e442c5b3
LeChat76 <cedric.delauney@gmail.com>
2023-02-07 15:02:47 +0100

    avancée mineure

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 
 def main():
     """ launching starting here """
+    from controllers.base import ChessTournament
     tournament = ChessTournament()
     try:
         tournament.tournament_start()
@@ -14,9 +15,8 @@ if __name__ == "__main__":
     DATA_FOLDER = os.path.join(os.path.dirname(__file__), "data", "tournaments")
     if not os.path.exists(DATA_FOLDER):
         os.makedirs(DATA_FOLDER)
-        with open(os.path.join(DATA_FOLDER, "players.json")):
-            pass
-        with open(os.path.join(DATA_FOLDER, "tournaments.json")):
-            pass
-    from controllers.base import ChessTournament
+        with open(os.path.join(DATA_FOLDER, "players.json"), 'w') as players_file:
+            players_file.write("")
+        with open(os.path.join(DATA_FOLDER, "tournaments.json"), 'w') as tournaments_file:
+            tournaments_file.write("")
     main()
